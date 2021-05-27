bf04e2a93086c2c540ae2558a2a4669312d85be5
pattylbrockman <84106924+pattylbrockman@users.noreply.github.com>
2021-05-27 09:11:24 -0500

    Create bult-in-min-max.py

---
+++ bult-in-min-max.py
@@ -0,0 +1,20 @@
+def maximum(value1, value2, value3):
+    """Return the maxiumum of three values."""
+     max_value=value1
+     if value2>max_value:
+             max_value=value2
+     if value3>max_value:
+             max_value=value3
+     return max_value
+maximum(12, 27, 36)
+max(12, 27, 36)
+def minimum(value1, value2, value3):
+     """Return the minimum of three values."""
+     min_value=value1
+     if value2<min_value:
+             min_value=value2
+     if value3<min_value:
+             min_value=value3
+     return min_value
+  minimum(15, 9, 27)
+  min(15, 9, 27)
